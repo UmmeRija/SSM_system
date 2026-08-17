b9474eaf9c24dfa269cb733641534599b58e31cd
UmmeRija <ummerijababar@gmail.com>
2026-08-16 19:47:04 -0700

    sec commit

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ ConnectDB()
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
+app.get("/", (req, res) => {
+
+    res.status(200).json({
+        success: true,
+        message: "SmartSociety Backend API is running...",
+        version: "1.0.0"
+    });
+
+});
 app.use("/api/user", UserRoutes)
 app.use("/api/visitor", VisitorRoutes)
 app.use("/api/maintenance", MaintenanceBillRoutes)
